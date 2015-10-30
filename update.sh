377d9de4381b3ee55e8916b24fdd533cae1470b7
c1phercraft <ciphercraft@gmail.com>
2015-10-30 20:30:20 +0100

    Added better output to the update script

--- update.sh
+++ update.sh
@@ -2,18 +2,20 @@
 
 cd "$(dirname "${BASH_SOURCE}")";
 
+echo "* Updating your local dot files with the remote ones."
+
 git pull origin master;
 
 function update() {
+    echo "* Syncing your dot files."
     rsync -i --exclude ".git/" --exclude "update.sh" --exclude "README.md" --exclude "LICENSE" --exclude "*.swp" -avh --no-perms . ~;
 }
 
-echo "Updating you local dot files with the remote ones."
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
     update;
 else
-    read -p "  Are you sure? (y/n) " -n 1;
+    read -p "Are you sure? (y/n) " -n 1;
     echo "";
     if [[ $REPLY =~ ^[Yy]$ ]]; then
         update;
