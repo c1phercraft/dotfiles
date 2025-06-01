c9d62c297e27466765957a6e85576318b16647af
c1phercraft <ingo+github@freedom.nl>
2025-06-01 14:55:11 +0200

    Update setup.sh to use new dgit command instead of dotfiles
    I found typing dotfiles each time too cumbersome.

--- setup.sh
+++ setup.sh
@@ -5,23 +5,22 @@ dotdir_backup=.dotfiles-backup
 git clone --bare git@github.com:c1phercraft/dotfiles.git $HOME/$dotdir
 
 # Prep the command to use.
-function dotfiles {
+function dgit {
    /usr/bin/git --git-dir=$HOME/$dotdir/ --work-tree=$HOME $@
 }
 
 # Try to checkout the repo.
 mkdir -p ${dotdir_backup}
-dotfiles checkout
+dgit checkout
 
 if [ $? != 0 ]; then
   # Some dotfiles already exist; let's backup them.
   echo "Backing up pre-existing dotfiles."
-  dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} ${dotdir_backup}/{}
+  dgit checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} ${dotdir_backup}/{}
 
   # And try the checkout again.
-  dotfiles checkout
+  dgit checkout
 fi
 
-dotfiles config status.showUntrackedFiles no
-echo "Dotfiles ready."
-
+dgit config status.showUntrackedFiles no
+echo "Dotfiles ready. Use 'dgit' command."
