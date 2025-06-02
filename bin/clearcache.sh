87d6402f3e68ea401225e0a345212056e05ac301
Ingo Haumann <Ingo.Haumann@tomtom.com>
2025-06-02 11:15:12 +0200

    Remember how to clear swap space

--- bin/clearcache.sh
+++ bin/clearcache.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 echo Clearing PageCache
 sync;sync;sync
+#sudo sh -c "/usr/bin/echo 1 > /proc/sys/vm/drop_caches && swapoff -a && swapon -a && printf '\n%s\n' 'RAM-cache and swap space cleared'"
 sudo sh -c "echo 1 > /proc/sys/vm/drop_caches"
-
